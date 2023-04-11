1ecdfaa71cb2ba1c8befb38262d03b9510b171ca
pranav112 <rpranav174@gmail.com>
2023-04-11 17:23:53 +0530

    Update performSCA.groovy

--- vars/performSCA.groovy
+++ vars/performSCA.groovy
@@ -2,7 +2,7 @@ def call() {
   echo "Performing SCA"
   installDependencies()
   DART_ANALYSIS_STATUS = sh(script: """
-      dart run dart_code_metrics:metrics analyze lib --reporter=html --set-exit-on-violation-level=alarm --no-fatal-style --fatal-performance --no-fatal-warnings 
+      flutter pub run dart_code_metrics:metrics analyze lib --reporter=html --set-exit-on-violation-level=alarm --no-fatal-style --fatal-performance --no-fatal-warnings 
   """, returnStatus: true)
   if (DART_ANALYSIS_STATUS != 0) {
       throw new Exception("Static code analysis failed with exit code ${DART_ANALYSIS_STATUS}")
